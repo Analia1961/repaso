238ac25c7037ed5dfe001cb7c212b1c23a512000
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-06-04 12:10:41 -0300

    ensayoPrueba-yError error error error error error error error error error error error error error error error error error error error error error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,71 @@
-//Modularización y Métodos (Repaso)
+/*Hoy comenzaremos con el ejercicio práctico de la diapositiva Nº52 
+(de PROG - 7. Repaso.pdf) con una sutil diferencia en el enunciado; 
+es decir, cambiaremos levemente el requerimiento 
+y adaptaremos el código al cambio solicitado.*/
 
-/* ejemplo 2
+// 2-Recibe un texto y lo convierte en un nombre de variable estilo CamelCase
+function convertirACamelCase(texto: string): string {
+  let textoLocal: string = texto.toLowerCase().trim();
+  let vbleCamelCase: string = "";
+  let indice: number = 0;
+  while (indice < textoLocal.length) {
+    if (textoLocal[indice] === " ") {
+      vbleCamelCase += textoLocal[indice + 1].toUpperCase();
+      indice += 2;
+    } else {
+      vbleCamelCase += textoLocal[indice];
+      indice++;
+    }
+  }
+  return vbleCamelCase;
+}
+let texto: string = prompt("Ingrese el texto a convertir");
+console.log("Texto original: " + texto);
+console.log("Texto camelCase: " + convertirACamelCase(texto));
+
+//------------- ✿◕‿◕✿----------------
+
+/* • Modifique el código del Ejercicio Identificación mes 
+utilizando arreglos
+Longitud = Length= 12 
+
+let miArreglo: string[] = new Array(12);
+
+miArreglo[0] = "Enero";
+miArreglo[1] = "Febrero";
+miArreglo[2] = "Marzo";
+miArreglo[3] = "Abril";
+miArreglo[4] = "Mayo";
+miArreglo[5] = "Junio";
+miArreglo[6] = "Julio";
+miArreglo[7] = "Agosto";
+miArreglo[8] = "Septiembre";
+miArreglo[9] = "Octubre";
+miArreglo[10] = "Noviembre";
+miArreglo[11] = "Diciembre";
+
+console.log(miArreglo);*/
+
+/*--------------------------
+Estructuras de Datos – Arreglos
+//Ejercicio – Arreglo de Números - Código
+// Algoritmo ArregloNumeros
+let num: number[] = new Array(7);
+let indice: number = 0;
+num[0] = 20;
+num[1] = 14;
+num[2] = 8;
+num[3] = 0;
+num[4] = 5;
+num[5] = 19;
+num[6] = 24;
+while (indice < 7) {
+  console.log("El número en la posición ", indice, " es ", num[indice]);
+  indice++;
+}*/
+
+/*----------------------------ejemplo 2
+
 let mensaje: string = "Hola lGloba!!";
 ambitoVariables();
 
@@ -15,13 +80,16 @@ function leeVariable() {
   console.log(mensaje);
 }*/
 
-/*ejemplo 1
+/*----------------------------ejemplo 1
+
 let mensaje: string = "Hola Global!!";
 ambitoVariables();
 
 //Ámbito de las Variables - Ejemplos
 function ambitoVariables() {
   let mensaje: string;
-  mensaje = "Hola Global!!";
+  mensaje = "Hola Mundo!!";
   console.log(mensaje);
 }*/
+
+//------------- ✿◕‿◕✿----------------fin
