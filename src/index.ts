a5ba03b1ea751fb0efeb02da272bf64ae05b753e
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-06-04 10:18:47 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+//Modularización y Métodos (Repaso)
+
+/* ejemplo 2
+let mensaje: string = "Hola lGloba!!";
+ambitoVariables();
+
+//ambitoVariables();
+
+function ambitoVariables() {
+  leeVariable();
+}
+function leeVariable() {
+  let mensaje: string;
+  mensaje = "Hola Mundo!!";
+  console.log(mensaje);
+}*/
+
+/*ejemplo 1
+let mensaje: string = "Hola Global!!";
+ambitoVariables();
+
+//Ámbito de las Variables - Ejemplos
+function ambitoVariables() {
+  let mensaje: string;
+  mensaje = "Hola Global!!";
+  console.log(mensaje);
+}*/
